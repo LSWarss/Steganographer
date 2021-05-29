602a5bbc843cf8c9f02972b444850699ca92578c
LSWarss <ls.warss98@gmail.com>
2021-05-29 11:39:24 +0200

    Initial Commit

---
+++ Steganographer/SteganographerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteganographerApp.swift
+//  Steganographer
+//
+//  Created by Łukasz Stachnik on 29/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SteganographerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
